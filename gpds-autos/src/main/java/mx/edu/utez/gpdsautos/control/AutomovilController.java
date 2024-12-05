f9c403599c459a3c6dd0021f806fcb9f3fa19a75
irauldominguezb <20213tn103@utez.edu.mx>
2024-12-04 20:17:59 -0600

    hehe

--- gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
+++ gpds-autos/src/main/java/mx/edu/utez/gpdsautos/control/AutomovilController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/automovilesss")
+@RequestMapping("/api/auto")
 public class AutomovilController {
 
     @Autowired
